ceab39398fae0afd2e4dc2ae93866ae12d4085b5
Zhenya <em167116@gmail.com>
2025-08-14 16:12:03 +0300

    edit README.md

--- src/Action.php
+++ src/Action.php
@@ -17,6 +17,11 @@ class Action
         $this->condition = $condition;
     }
 
+    /**
+     * Устанавливает обработчик для маршрута.
+     *
+     * @param callable $handler Обработчик
+     */
     public function func(callable $handler): self
     {
         $this->handler = \Closure::fromCallable($handler);
@@ -24,6 +29,14 @@ class Action
         return $this;
     }
 
+    /**
+     * Перенаправляет один маршрут на другой.
+     * Копирует обработчик и данные ответа из маршрута $id в текущий маршрут.
+     *
+     * @param string $id    ID маршрута, куда перенаправлять
+     *
+     * @throws \InvalidArgumentException Если один из маршрутов не найден.
+     */
     public function redirect(string $id): self
     {
         $this->button_redirect = $id;
@@ -31,6 +44,11 @@ class Action
         return $this;
     }
 
+    /**
+     * Задает текст сообщения, которое будет отправлено в ответ
+     *
+     * @param string $text    Текст сообщения
+     */
     public function text(string $text = ''): self
     {
         $this->messageData['text'] = $text;
@@ -38,6 +56,11 @@ class Action
         return $this;
     }
 
+    /**
+     * Изменяет текст сообщения
+     *
+     * @param string $text    Новый текст
+     */
     public function editText(string $text = ''): self
     {
         $this->messageData['text'] = $text;
@@ -46,6 +69,11 @@ class Action
         return $this;
     }
 
+    /**
+     * Изменяет текст описания
+     *
+     * @param string $text    Новый текст
+     */
     public function editCaption(string $text = ''): self
     {
         $this->messageData['text'] = $text;
@@ -54,18 +82,35 @@ class Action
         return $this;
     }
 
-    public function img(string $img): self
+    /**
+     * Добавляет изображение к сообщению
+     *
+     * @param string|array $img    Ссылка или массив ссылок (ID) изображений
+     */
+    public function img(string|array $img): self
     {
         $this->messageData['img'] = $img;
 
         return $this;
     }
 
+    /**
+     * Задает всплывающий текст при нажатии на кнопку
+     *
+     * @param string $query    Всплывающий текст
+     */
     public function query(string $query): self
     {
         return $this->setQueryText($query);
     }
 
+    /**
+     * Добавляет клавиатуру к сообщению
+     *
+     * @param array $buttons    Кнопки клавиатуры
+     * @param bool  $one_time   Показывать клавиатуру однократно?
+     * @param bool  $resize     Растягивать клавиатуру?
+     */
     public function kbd(array $buttons, bool $one_time = false,
         bool $resize = true,
     ): self {
@@ -77,6 +122,11 @@ class Action
         return $this;
     }
 
+    /**
+     * Добавляет inline-клавиатуру к сообщению
+     *
+     * @param array $buttons    Кнопки клавиатуры
+     */
     public function inlineKbd(array $buttons): self
     {
         $this->messageData['kbd'] = $buttons;
@@ -87,6 +137,9 @@ class Action
         return $this;
     }
 
+    /**
+     * Удаляет клавиатуру
+     */
     public function removeKbd(): self
     {
         $this->messageData['remove_keyboard'] = true;
